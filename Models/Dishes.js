26baafb5f680f5eedfc57571f2e8e4ef52659ec3
yossef-khaled <yossef.k.y333@gmail.com>
2020-09-12 13:47:19 +0200

    Added sub doc 'comment' into dishes.

--- Models/Dishes.js
+++ Models/Dishes.js
@@ -1,6 +1,25 @@
 const mongoose = require('mongoose');
 const schema = mongoose.Schema;
 
+const commentSchema = new schema({
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    comment: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true  
+    }
+},{
+    timestamps: true
+});
+
 const disheSchema = new schema({
     name: {
         type: String,
@@ -10,7 +29,8 @@ const disheSchema = new schema({
     description: {
         type: String,
         required: true
-    }
+    },
+    comments: [commentSchema]
 },
 {
     timestamps: true    
